cd3d0f6ec8b6c38bc1423b23a019b3210ff16b4a
TheIthorian <ben.shortland1020@gmail.com>
2023-12-03 12:35:20 +0000

    feat: initial commit

---
+++ prettier.config.cjs
@@ -0,0 +1,8 @@
+/** @type {import("prettier").Config} */
+module.exports = {
+    arrowParens: 'avoid',
+    htmlWhitespaceSensitivity: 'strict',
+    jsxSingleQuote: true,
+    singleQuote: true,
+    tabWidth: 4,
+};
---
+++ src/__tests__/index.ts
@@ -0,0 +1,5 @@
+describe('A test', () => {
+    it('should pass', () => {
+        expect(true).toBe(true);
+    });
+});
